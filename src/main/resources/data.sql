55bd2f5d069a0fd08c2f138216abdb8c5ab81aff
bruno-gazola <bruninhogazola2@gmail.com>
2025-06-05 13:22:29 -0300

    Mudança para underline no data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,10 @@
-INSERT IGNORE INTO tb_tipo_usuario (idTipoUsuario, nomeTipoUsuario) VALUES
+INSERT IGNORE INTO tb_tipo_usuario (id_tipo_usuario, nome_tipo_usuario) VALUES
 (1, 'Admin'),
 (2, 'Cliente'),
 (3, 'Vendedor'),
 (4, 'Gerente');
 
-INSERT IGNORE INTO tb_categoria_produto (idCategoriaProduto, nomeCategoriaProduto, descricaoCategoriaProduto) VALUES
+INSERT IGNORE INTO tb_categoria_produto (id_categoria_produto, nome_categoria_produto, descricao_categoria_produto) VALUES
 (1, 'Anéis', 'Acesório para os dedos'),
 (2, 'Colares', 'Acesório para o pescoço'),
 (3, 'Brincos', 'Acesório para as orelhas'),
@@ -12,18 +12,19 @@ INSERT IGNORE INTO tb_categoria_produto (idCategoriaProduto, nomeCategoriaProdut
 (5, 'Relógios', 'Acesório para não atrasar no SENAI');
 
 -- Inserindo Tipos (se não existirem)
-INSERT IGNORE INTO tb_tipo_produto (idTipoProduto, nomeTipoProduto, descricaoTipoProduto) VALUES
+INSERT IGNORE INTO tb_tipo_produto (id_tipo_produto, nome_tipo_produto, descricao_tipo_produto) VALUES
 (1, 'Ouro', '18k'),
 (2, 'Prata', '750'),
 (3, 'Bijuteria', 'É biju mas é de rico');
 
 -- inserindo a tb_ornamento
-INSERT IGNORE INTO tb_ornamento (idOrnamento, descricao, nome) VALUES
+INSERT IGNORE INTO tb_ornamento (id_ornamento, descricao, nome) VALUES
 (1, 'Ornamento de ouro com pedras preciosas', 'Anel de Ouro'),
 (2, 'Ornamento de prata com design moderno', 'Brinco de Prata'),
 (3, 'Ornamento com pedras sintéticas', 'Colar de Cristal');
 
 -- Colocando 5 clientes ja cadastrados 
+<<<<<<< HEAD
 <<<<<<< HEAD
 INSERT IGNORE INTO tb_usuario (id_usuario, nome_usuario, cpf, email, senha,  telefone, dt_nascimento, tipo_usuario)VALUES
 (1, 'Khallil','47456147896','khalli@gmail.com','12345','996141865','1999-02-05','Cliente',),
@@ -57,6 +58,9 @@ INSERT IGNORE INTO tb_produto(id_produto, nome, desc_produto, preco, quant_estoq
 (20,"Anel de Jade","Anel de jade montado em ouro com acabamento brilhante",650,50,"Anel","Jade");
 =======
 INSERT IGNORE INTO tb_usuario (idUsuario, nomeUsuario, cpf, email, senha,  telefone, dtNascimento, tipoUsuario)VALUES
+=======
+INSERT IGNORE INTO tb_usuario (id_usuario, nome_usuario, cpf, email, senha,  telefone, dt_nascimento, tipo_usuario)VALUES
+>>>>>>> 1cb8f5e (Mudança para underline no data.sql)
 (1, 'Khallil','47456147896','khalli@gmail.com','12345','996141865','1999-02-05','Cliente'),
 (2, 'Gariella','45678468168','gabi@gmail.com','23451','997627706','1999-08-06','Cliente'),
 (3, 'Rafaela','45678912465','rafa@gmail.com','34512','998756423','2001-01-25','Cliente'),
